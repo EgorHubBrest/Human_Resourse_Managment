f73af085b24ccb40f767e7f304ba963659a669c5
EgorHubBrest <egorkam10@gmail.com>
2021-02-27 18:29:15 +0300

    fix pylint

--- hrm/department_app/tests/tests.py
+++ hrm/department_app/tests/tests.py
@@ -1,4 +1,4 @@
 """Django Test"""
-from django.test import TestCase 
+#from django.test import TestCase
 
 # Create your tests here.
--- hrm/department_app/views/views.py
+++ hrm/department_app/views/views.py
@@ -1,4 +1,4 @@
 """Django views"""
-from django.shortcuts import render 
+#from django.shortcuts import render
 
 # Create your views here.
