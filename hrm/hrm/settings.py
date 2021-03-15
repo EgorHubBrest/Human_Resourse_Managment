ae78bdf6e5ae18cdb5dfa50dc5d34053e28a6cc7
EgorHubBrest <egorkam10@gmail.com>
2021-03-15 19:57:48 +0300

    update

--- hrm/hrm/settings.py
+++ hrm/hrm/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'hrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_project',
         'USER': 'django_user',
         'PASSWORD': 'qwerty',
