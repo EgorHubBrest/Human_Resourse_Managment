d963786ad0c1a1b80cfacae67be7c58fe3e80b28
EgorHubBrest <egorkam10@gmail.com>
2021-04-03 17:15:29 +0300

    add

--- hrm/department_app/serializers.py
+++ hrm/department_app/serializers.py
@@ -1,3 +1,4 @@
+"""Serializer"""
 from rest_framework import serializers
 
 from department_app.models.department_models import Department
@@ -6,13 +7,17 @@ from department_app.models.employee_model import Employee
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
+    """DepartmentTable"""
     class Meta:
+        """MetaClass"""
         model = Department
         fields = ['name', 'status']
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
+    """EmployeeTable"""
     class Meta:
+        """MetaClass"""
         model = Employee
         fields = ['id', 'name', 'department',
                   'date', 'salary', 'email', 'status']
